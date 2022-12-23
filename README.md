6f622533c585032a995a90c87657385a149b972b
Marnick van der Arend <marnick@vdarend.com>
2022-12-23 15:15:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LAMP-Java17
---
+++ src/main/java/nl/utwente/student/example/Main.java
@@ -0,0 +1,5 @@
+package nl.utwente.student.example;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
